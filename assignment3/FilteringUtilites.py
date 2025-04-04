d64ac5c9ac841fbec2fa75a4142e71cf6f30e341
apoirier <netid@student.tudelft.nl>
2025-04-04 18:28:13 +0200

    Code of the time filter. This was not tested yet, it must still be added in the main loop.

--- assignment3/FilteringUtilites.py
+++ assignment3/FilteringUtilites.py
@@ -1,3 +1,6 @@
+import numpy as np
+from astropy.coordinates.earth_orientation import eccentricity
+import tudatpy.astro.element_conversion as ec
 
 def apogee_perigee_filter(rso1, rso2, D):
     """
@@ -43,6 +46,78 @@ def geometrical_filter(rso1, rso2, D):
         return False  # Keep for further analysis
     return True  # Filter out
 
+
+def time_filter(rso1, rso2, D):
+    IR = np.abs(rso1['inclination'] - rso2['inclination'])
+    mu = 3.986 * 10 ** 14
+    ######  Object one #######
+    a_1 = (rso1['perigee'] + rso1['apogee'])/2
+    e_1 = (rso1['perigee'] - rso1['apogee'])/(rso1['perigee'] + rso1['apogee'])
+
+    #Constants
+    alpha_1 = a_1* (1-e_1**2) * np.sin(IR)
+    ax_1 = e_1 * np.cos(rso1['argument_of_perigee']- rso1['raan'])
+    ay_1 = e_1 * np.sin(rso1['argument_of_perigee']- rso1['raan'])
+    Q_1 = alpha_1 * (alpha_1 - 2*D*ay_1) - (1-e_1**2)*D**2
+
+    to_solve_1 = (-D**2*ax_1 + (alpha_1-D*ay_1)*np.sqrt(Q_1))/(alpha_1*(alpha_1 - 2*D*ay_1)+D**2*e_1**2)
+    to_solve_2 = (-D**2*ax_1 - (alpha_1-D*ay_1)*np.sqrt(Q_1))/(alpha_1*(alpha_1 - 2*D*ay_1)+D**2*e_1**2)
+    if Q_1 < 0 or np.abs(to_solve_1) > 1 or np.abs(to_solve_2) > 1 :
+        return False
+    ur1 = np.arccos(to_solve_1)
+    ur2 = np.arccos(to_solve_2)
+
+    ######  Object two #######
+    a_2 = (rso2['perigee'] + rso2['apogee']) / 2
+    e_2 = (rso2['perigee'] - rso2['apogee']) / (rso2['perigee'] + rso2['apogee'])
+
+    # Constants
+    alpha_2 = a_2*(1 - e_2 ** 2) * np.sin(IR)
+    ax_2 = e_2 * np.cos(rso2['argument_of_perigee'] - rso2['raan'])
+    ay_2 = e_2 * np.sin(rso2['argument_of_perigee'] - rso2['raan'])
+    Q_2 = alpha_2 * (alpha_2 - 2 * D * ay_2) - (1 - e_2 ** 2) * D ** 2
+
+
+    to_solve_3 = (-D ** 2 * ax_2 + (alpha_2 - D * ay_2) * np.sqrt(Q_2)) / (
+                alpha_2 * (alpha_2 - 2 * D * ay_2) + D ** 2 * e_2 ** 2)
+    to_solve_4 = (-D ** 2 * ax_2 - (alpha_2 - D * ay_2) * np.sqrt(Q_2)) / (
+                alpha_2 * (alpha_2 - 2 * D * ay_2) + D ** 2 * e_2 ** 2)
+
+    if Q_2 < 0 or np.abs(to_solve_3) > 1 or np.abs(to_solve_4) > 1 :
+        return False
+
+    ur3 = np.arccos(to_solve_3)
+    ur4 = np.arccos(to_solve_4)
+
+    #
+    f1 = ur1 - rso1['argument_of_perigee'] - rso1['raan']
+    f2 = ur2 - rso1['argument_of_perigee'] - rso1['raan']
+    f3 = ur3 - rso2['argument_of_perigee'] - rso2['raan']
+    f4 = ur4 - rso2['argument_of_perigee'] - rso2['raan']
+
+
+    M1 = ec.true_to_mean_anomaly(e_1,f1)
+    t1 = ec.delta_mean_anomaly_to_elapsed_time(M1,mu,a_1*10**3)
+
+    M2 = ec.true_to_mean_anomaly(e_1, f2)
+    t2 = ec.delta_mean_anomaly_to_elapsed_time(M2, mu, a_1*10**3)
+
+    M3 = ec.true_to_mean_anomaly(e_2, f3)
+    t3 = ec.delta_mean_anomaly_to_elapsed_time(M3, mu, a_2*10**3)
+
+    M4 = ec.true_to_mean_anomaly(e_2, f4)
+    t4 = ec.delta_mean_anomaly_to_elapsed_time(M4, mu, a_2*10**3)
+
+    T1 = 2*np.pi*np.sqrt((a_1*10**3)**3/mu)
+    T2 = 2*np.pi*np.sqrt((a_2*10**3)**3/mu)
+
+    n = 0
+    while n*T1 < 48 * 3600:
+        if max(T1*n + t1,T2*n + t3) < min(T1*n + t2,T2*n +t4):
+            return False
+        else:
+            n = n + 1
+    return True
 
 def filter_object_pairs(rso_catalog, D):
     """
